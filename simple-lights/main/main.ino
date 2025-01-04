51b53b57ac52b3c8d695b2d33322b7c35f0097de
Russell Palmiter <russell.palmiter@vorto.ai>
2025-01-03 22:09:52 -0700

    working broker

---
+++ simple-lights/main/main.ino
@@ -0,0 +1,28 @@
+#include <FastLED.h>
+
+#define DATA_PIN 5
+#define NUM_LEDS 200
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+}
+
+void loop() {
+  // Set all LEDs to red
+  fill_solid(leds, NUM_LEDS, CRGB::Red);
+  FastLED.show();
+  delay(1000);
+
+  // Set all LEDs to green
+  fill_solid(leds, NUM_LEDS, CRGB::Green);
+  FastLED.show();
+  delay(1000);
+
+  // Set all LEDs to blue
+  fill_solid(leds, NUM_LEDS, CRGB::Blue);
+  FastLED.show();
+  delay(1000);
+}
+
